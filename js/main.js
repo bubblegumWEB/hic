d47f5596b32e81376cc8e54add168d50d7816e9d
bubblegumWEB <gregorickliriano@bubblegum.do>
2022-06-29 13:13:23 -0400

    button caja fixed

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const subsidiariaCajaCerrar = document.querySelectorAll(
 subsidiariaCaja.forEach((caja) => {
   caja.addEventListener("click", (e) => {
     subsidiariaCajaInfo.forEach((cajaInfo) => {
-      if (e.target.id === cajaInfo.dataset.cajainfo) {
+      if (e.target.dataset.cajainfo === cajaInfo.dataset.cajainfo) {
         cajaInfo.classList.remove("d-none");
       }
       subsidiariaCajaCerrar.forEach((butoncerrar) => {
